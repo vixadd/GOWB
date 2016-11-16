c1f1029d008dfd8205b9ec03dcea2a72222a3cfc
vixadd <dkroell@acm.org>
2016-11-16 09:02:49 -0500

    Added some functionality to baker

--- Baker/Baker.cpp
+++ Baker/Baker.cpp
@@ -3,18 +3,32 @@
 #include "..\includes\Baker.h"
 using namespace std;
 
-Baker::Baker(int id):id(id)
-{
-}
+Baker::Baker(int id):id(id) { }
 
-Baker::~Baker()
-{
-}
+Baker::~Baker() { }
 
 void Baker::bake_and_box(ORDER &anOrder) {
-	
+	while ( anOrder.number_donuts > 0 ) {
+		if( anOrder.number_donuts % DOZEN == 0 ) {
+			anOrder.boxes.insert(box);
+			Box box = new Box();
+		}
+		DONUT d;
+		box.addDonut(d);
+		anOrder.number_donuts = anOrder.number_donuts -1;
+	}
+	anOrder.boxes.insert(box);
 }
 
 void Baker::beBaker() {
-	
+	while(order_in_Q.size() > 0) {
+		ORDER o = order_in_Q.front();
+		order_in_Q.pop();
+
+		//Lock it here
+		bake_and_box(o);
+		order_outvector.insert(o);
+
+		// Check condition variable
+	}
 }
