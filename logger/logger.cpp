a90dc29e45e641a3343292bf1e5c95db46cd4ce6
vixadd <dkroell@acm.org>
2016-11-19 16:54:56 -0500

    added Logger file

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -1,7 +1,8 @@
-#include "../includes/logger.h"
+#include "..\includes\logger.h"
 using namespace std;
 
 Logger::Logger(std::string filename) {
+	//No critical section here since this is only called once
 	this->filename = filename;
 }
 
@@ -10,6 +11,10 @@ Logger::~Logger() {
 
 //open close and clear the log file
 void Logger::clearlogfile() {
+	//lock the whole clearlogfile function to prevent 
+	//multiple changes to the same file at the same time.
+	//if an exception is thrown, the mutex will automatically unlock
+	lock_guard<mutex> lock(m);
 	myFile.open(filename, std::fstream::trunc);
 
 	//close file
@@ -18,6 +23,10 @@ void Logger::clearlogfile() {
 }
 
 void Logger::log(std::string data) {
+	//lock the whole log function to prevent
+	//multiple changes to the same file at the same time.
+	//if an exception is thrown, the mutex will automatically unlock
+	lock_guard<mutex> lock(m);
 	myFile.open(filename, std::fstream::app);
 	if (!myFile.is_open())
 		return;
